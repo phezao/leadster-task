c96c06ae27842906c8c44536e3ff8bf8a3f6eef6
Philip Oka <philip.oka@gmail.com>
2022-08-26 20:54:49 -0300

    - Changing factories from directory - Creating the contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,57 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: %i[show edit update destroy]
+
+  def index
+    @contacts = ContactSearcher.call(params[:fullname])
+  end
+
+  def show
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def edit
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to contact_path(@example), notice: "Contato criado com sucesso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to contact_path(@contact), notice: "Contato atualizado com sucesso."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact.destroy
+
+    redirect_to examples_url, notice: "Contato excluído com sucesso."
+  end
+
+  private
+
+  def set_contact
+    @contact = contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(
+      :fullname,
+      :tax_id,
+      :email,
+      :birthdate,
+      address_attributes: %i[zipcode city state street street_number neighborhood]
+    )
+  end
+end
